f00b072bd2901e2cc93e60f33b00879c3a6b1720
Bajiraoo512 <jayeshpatil665@gmail.com>
2021-05-22 13:41:51 +0530

    secondCommit_checkInternetConnectivity_onAppSpart

--- app/src/main/java/in/specialsoft/securesteps/SplashScreen.java
+++ app/src/main/java/in/specialsoft/securesteps/SplashScreen.java
@@ -1,9 +1,14 @@
 package in.specialsoft.securesteps;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -11,9 +16,26 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (isNetworkConnected())
+            {
+                Intent noError = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(noError);
+            }
+            else
+            {
+                Toast.makeText(this, "Internet Not Available", Toast.LENGTH_SHORT).show();
+                Intent networkError = new Intent(SplashScreen.this,NetworkErrorActivity.class);
+                startActivity(networkError);
+            }
+        }
 
-        Intent intent = new Intent(SplashScreen.this,MainActivity.class);
-        startActivity(intent);
-        finish();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
     }
 }

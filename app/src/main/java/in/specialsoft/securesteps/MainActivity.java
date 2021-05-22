fd195ecb0113940a6f505a49b62f21137ef72728
Bajiraoo512 <jayeshpatil665@gmail.com>
2021-05-22 18:30:58 +0530

    ThirdCommit_User_parent_sideDesign

--- app/src/main/java/in/specialsoft/securesteps/MainActivity.java
+++ app/src/main/java/in/specialsoft/securesteps/MainActivity.java
@@ -2,7 +2,10 @@ package in.specialsoft.securesteps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toHome(View view) {
+        Intent intent = new Intent(MainActivity.this,ParentActivity.class);
+        startActivity(intent);
+    }
+
+    public void toLocation(View view) {
+        Toast.makeText(this, "Getting Current Location", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/in/specialsoft/securesteps/ParentActivity.java
@@ -0,0 +1,14 @@
+package in.specialsoft.securesteps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent);
+    }
+}
